9df460c161b9b6cee46b90e65d7b66ae972d15e7
Spuja123 <45414373+Spuja123@users.noreply.github.com>
2023-07-29 21:53:01 +0530

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RoutingModule } from './app-route.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 @NgModule({
-  imports: [BrowserModule, RoutingModule, FormsModule, ReactiveFormsModule],
-  declarations: [
-    AppComponent,
-    SignInComponent,
-    MenuBarComponent,
-    HomeComponent,
-  ],
+  imports: [BrowserModule, RoutingModule, FormsModule],
+  declarations: [AppComponent, SignInComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
