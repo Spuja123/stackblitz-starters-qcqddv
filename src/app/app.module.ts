750c1d6962df71e5f9d4578f482b2e9fb5d8bcf5
Spuja123 <45414373+Spuja123@users.noreply.github.com>
2023-07-29 22:05:14 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RoutingModule } from './app-route.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 @NgModule({
   imports: [BrowserModule, RoutingModule, FormsModule],
-  declarations: [AppComponent, SignInComponent, HomeComponent],
+  declarations: [AppComponent, SignInComponent, NavBarComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent {
+  @Input()
+  name!: string;
+
+  isOpen = false;
+  isDropdownOpen = false;
+  constructor() {}
+
+  toggleNavbar() {
+    this.isOpen = !this.isOpen;
+  }
+
+  toggleDropDown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+}
