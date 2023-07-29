b541f38ae7b9f4019a2b0e2565dca4b609f8a348
Spuja123 <45414373+Spuja123@users.noreply.github.com>
2023-07-29 21:44:40 +0530

    7 files modified

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
